86258e5d5ef5285efe4de227fe88904b8e46ff01
mangomarcus <mdhudson132@gmail.com>
2024-04-14 15:29:50 +0100

    Replicate issue

--- src/app/[[...slug]]/page.tsx
+++ src/app/[[...slug]]/page.tsx
@@ -1,20 +1,55 @@
-import Link from 'next/link';
-
 export function generateStaticParams() {
   return [
     {
       slug: [], // ie. /
     },
     {
-      slug: ['foo'], // ie. foo/bar
+      slug: ['foo'], // ie. /foo
+    },
+    {
+      slug: ['foo/bar'], // ie. /foo/bar
     },
   ]
 }
 
-export default function Page({params}: {
+export default async function Page({params}: {
   params: {
     slug: string[] | undefined;
   };
 }) {
-  return <ul><li><Link href="/">Home</Link></li><li><Link href="/foo">Foo</Link></li></ul>;
+  const { slug = [] } = params;
+
+  /**
+   * Page URI without leading slash. Home page should be `'/'`. On Vercel it's `'index'` but only when navigating with `<Link href="/">`.
+   */
+  const uri = getUriFromSegments(slug);
+  const page = await getPageWithUri(uri);
+
+  return <p>{page ? page.title : 'NOT FOUND'} - {uri}</p>;
+}
+
+function getUriFromSegments(slug: string[]) {
+  if (slug.length === 0) {
+    return '/';
+  }
+  return slug.join('/');
+}
+
+async function getPageWithUri(uri: string) {
+  return uri in pages ? pages[uri as keyof typeof pages] : null;
+}
+
+/**
+ * Imagine these pages are fetched from a database.
+ */
+const pages = {
+  '/': {
+    title: 'Home',
+  },
+  'foo': {
+    title: 'Foo',
+  },
+  'foo/bar': {
+    title: 'Bar',
+  },
 }
